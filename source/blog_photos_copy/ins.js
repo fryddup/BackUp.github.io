85c7afb12c35251222c2b45d20198bbd612ed534
fryddup <1443918219@qq.com>
2019-09-06 23:43:13 +0800

    decriptions

--- source/blog_photos_copy/ins.js
+++ source/blog_photos_copy/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/fryddup/BackUp/master/source/min_photos/' + data.link[i] ;
-          var src = 'https://raw.githubusercontent.com/fryddup/ackUp/master/source/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/fryddup/BackUp.github.io/master/source/min_photos/' + data.link[i] ;
+          var src = 'https://raw.githubusercontent.com/fryddup/BackUp.github.io/master/source/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
